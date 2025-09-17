8c9200c587fb60a2d309dd7a5babdfea2595db7f
hrems-dev <hrems077@gmail.com>
2025-09-17 00:18:17 -0500

    cambio en js y css

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
-const item1 = document.getElementById('fig1');
-const item2 = document.getElementById('fig2');
-const item3 = document.getElementById('fig3');
-function hideAllItems() {
-    item1.style.display = 'none';
-    item2.style.display = 'none';
-    item3.style.display = 'none';
+const item1 = document.getElementById('item1');
+const item2 = document.getElementById('item2');
+const item3 = document.getElementById('item3');
+const fig1 = document.getElementById('fig1');
+const fig2 = document.getElementById('fig2');
+const fig3 = document.getElementById('fig3');
+
+function resetItemColors() {
+    item1.style.backgroundColor = '';
+    item2.style.backgroundColor = '';
+    item3.style.backgroundColor = '';
 }
+
+const darkColor = '#46683aff';
+
 item1.addEventListener('click', () => {
-    hideAllItems();
-    console.log('click');
-    item1.style.display = 'block';
+    resetItemColors();
+    item1.style.backgroundColor = darkColor;
 });
+
 item2.addEventListener('click', () => {
-    hideAllItems();
-    console.log('click');
-    item2.style.display = 'block';
+    resetItemColors();
+    item2.style.backgroundColor = darkColor;
 });
+
 item3.addEventListener('click', () => {
-    hideAllItems();
-    item3.style.display = 'block';
+    resetItemColors();
+    item3.style.backgroundColor = darkColor;
+});
+item1.addEventListener('click', () => {
+    fig1.style.display = 'block';
+    fig2.style.display = 'none';
+    fig3.style.display = 'none';
+});
+
+item2.addEventListener('click', () => {
+    fig1.style.display = 'none';
+    fig2.style.display = 'block';
+    fig3.style.display = 'none';
+});
+
+item3.addEventListener('click', () => {
+    fig1.style.display = 'none';
+    fig2.style.display = 'none';
+    fig3.style.display = 'block';
 });
